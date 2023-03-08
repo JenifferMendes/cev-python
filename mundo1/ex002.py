ced11c669ab1c3ced12908b96dc9b993d1f8e172
Jeniffer Mendes <115995202+JenifferMendes@users.noreply.github.com>
2023-03-07 21:49:44 -0300

    feat: exercicio 2 do mundo 1

---
+++ mundo1/ex002.py
@@ -0,0 +1,4 @@
+dia = input('Qual o dia que você nasceu?')
+mes = input('Qual o mês que você nasceu?')
+ano = input('Qual o ano que você nasceu?')
+print(f'Sua data de nascimento é {dia}/{mes}/{ano}')
