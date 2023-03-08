8aee37648fdfe7893c5faf550bbfa6cb31d8a91a
Jeniffer Mendes <115995202+JenifferMendes@users.noreply.github.com>
2023-03-07 21:49:53 -0300

    feat: exercicio 3 do mundo 1

---
+++ mundo1/ex003.py
@@ -0,0 +1,5 @@
+''' Crie um script python que leia dois números e tente mostrar a soma entre eles. '''
+
+primeiro_numero = input('Digite um numero: ')
+segundo_numero = input('Digite outro numero: ')
+print(int(primeiro_numero) + int(segundo_numero))
