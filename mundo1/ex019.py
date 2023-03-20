568d848ff59e3284e22d03d4766f00887e4011b7
Jeniffer Mendes <115995202+JenifferMendes@users.noreply.github.com>
2023-03-20 20:41:09 -0300

    feat: desafio 19 do mundo 1

---
+++ mundo1/ex019.py
@@ -0,0 +1,18 @@
+"""Um professor quer sortear um dos seus quatro alunos para apagar o quadro.
+Faça um programa que ajude ele, lendo o nome delas e escrevendo o nome do escolhido"""
+
+
+import random
+
+
+aluno1 = input('Digite o nome do aluno 1: ')
+aluno2 = input('Digite o nome do aluno 2: ')
+aluno3 = input('Digite o nome do aluno 3: ')
+aluno4 = input('Digite o nome do aluno 4: ')
+alunos = [aluno1, aluno2, aluno3, aluno4 ]
+
+aluno_sorteado = random.choice(alunos)
+
+print(f'O aluno sorteado foi {aluno_sorteado}')
+#print(f'O aluno sorteado foi {alunos[aluno_sorteado]}')
+#print(f'{globals().get(f"aluno{aluno_sorteado}")}')
