8a104c647c0398fc3047fe36f037ee563d4bd0ef
Jeniffer Mendes <115995202+JenifferMendes@users.noreply.github.com>
2023-03-22 19:44:59 -0300

    uptade: lista separada dos input

--- mundo1/ex019.py
+++ mundo1/ex019.py
@@ -9,6 +9,7 @@ aluno1 = input('Digite o nome do aluno 1: ')
 aluno2 = input('Digite o nome do aluno 2: ')
 aluno3 = input('Digite o nome do aluno 3: ')
 aluno4 = input('Digite o nome do aluno 4: ')
+
 alunos = [aluno1, aluno2, aluno3, aluno4 ]
 
 aluno_sorteado = random.choice(alunos)
