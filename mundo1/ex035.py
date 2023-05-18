f7a3aafacbab1f2c59bdf1e4101b61db86179541
Jeniffer Mendes <115995202+JenifferMendes@users.noreply.github.com>
2023-05-18 19:05:51 -0300

    feat: desafio 35 feito

---
+++ mundo1/ex035.py
@@ -0,0 +1,14 @@
+"""Desenvolva um programa que leia o comprimento de três retas e diga ao usuário
+se elas podem ou não formar um triângulo."""
+
+
+reta_um = int(input('Digite o comprimento um: '))
+reta_dois = int(input('Digite o comprimento dois: '))
+reta_tres = int(input('Digite o comprimento tres: '))
+
+if (reta_um + reta_dois) > reta_tres:
+    if (reta_dois + reta_tres) > reta_um:
+        if (reta_um + reta_tres) > reta_dois:
+            print(f'Pode formar um triângulo')
+else:
+    print(f'Não pode formar um triângulo')
