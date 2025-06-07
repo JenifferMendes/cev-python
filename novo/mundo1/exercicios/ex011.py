af8be458ee7bcbb1d6476fe22dbf111157092d07
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-07 16:40:35 -0400

    fix: pep8 + pt

--- novo/mundo1/exercicios/ex011.py
+++ novo/mundo1/exercicios/ex011.py
@@ -1,6 +1,6 @@
 """
 Faça um programa que leia a largura e a altura de uma parece em metros,
-calcule a sua área  e a quantidade de tinta necessária para pintá-la,
+calcule a sua área e a quantidade de tinta necessária para pintá-la,
 sabendo que cada litro de tinta, pinta uma area de 2m².
 """
 
@@ -11,4 +11,4 @@ altura = float(input("Digite a altura: "))
 area = largura * altura
 tinta = area / 2
 
-print(f"A área que deseja pintar é {area}m e a tinta necessaria é {tinta}l")
+print(f"A área que deseja pintar é {area}m² e a tinta necessaria é {tinta}l")
