fba189d9705142352d5b7f01a83935c14fc79325
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-07 15:55:41 -0400

    feat: others units of measurement

--- novo/mundo1/exercicios/ex008.py
+++ novo/mundo1/exercicios/ex008.py
@@ -4,12 +4,26 @@ em centrimentros e minilimetros
 """
 
 
-metro = float(input("Qual é o valor em metros? "))
+medida = float(input("Uma distancia em metro? "))
 
-centimetro = metro * 1e2
-milimetro = metro * 1e3
+kilometro = medida * 1e-3
+hectometro = medida * 1e-2
+decametro = medida * 1e-1
+decimetro = medida * 1e1
+centimetro = medida * 1e2
+milimetro = medida * 1e3
+micrometro = medida * 1e6
+nanometro = medida * 1e9
+picometro = medida * 1e12
 
 print(
-    f"O valor digitado em metros é: {metro}"
-    f", isso é {centimetro}cm e {milimetro}mm"
+    f"O valor digitado em metros é: {medida} que é igual a:\n"
+    f"{kilometro:,.3f}km\n"
+    f"{hectometro:,.3f}hm\n"
+    f"{decametro:,.3f}dam\n"
+    f"{centimetro:,.0f}cm\n"
+    f"{milimetro:,.0f}mm\n"
+    f"{micrometro:,.0f}µm \n"
+    f"{nanometro:,.0f}nm \n"
+    f"{picometro:,.0f}pm \n"
 )
