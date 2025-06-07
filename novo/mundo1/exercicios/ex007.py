a893b2a4edece843159a5f6aa9abd05d703e2620
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-07 15:54:19 -0400

    uptade: number format

--- novo/mundo1/exercicios/ex007.py
+++ novo/mundo1/exercicios/ex007.py
@@ -4,9 +4,9 @@ a sua média.
 """
 
 
-nota1 = int(input("Digite sua nota: "))
-nota2 = int(input("Digite sua segunda nota: "))
+nota1 = float(input("Digite sua nota: "))
+nota2 = float(input("Digite sua segunda nota: "))
 
 media = (nota1 + nota2) / 2
 
-print(f"A média das notas {nota1} e {nota2} é igual a {media}")
+print(f"A média das notas {nota1} e {nota2} é igual a {media:.2f}")
