848337480f82d5716ba712333098b24410859765
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-07 16:22:21 -0400

    uptade: format

--- novo/mundo1/exercicios/ex009.py
+++ novo/mundo1/exercicios/ex009.py
@@ -6,16 +6,20 @@ a sua tabuada.
 
 num = int(input("Digite um número: "))
 
+enfeite = f"{15*"="}\n"
+
 print(
     f"Seu número é {num} e a tabuada:\n"
-    f"{num}X1 = {num * 1}\n"
-    f"{num}x2 = {num * 2}\n"
-    f"{num}x3 = {num * 3}\n"
-    f"{num}x4 = {num * 4}\n"
-    f"{num}x5 = {num * 5}\n"
-    f"{num}x6 = {num * 6}\n"
-    f"{num}x7 = {num * 7}\n"
-    f"{num}x8 = {num * 8}\n"
-    f"{num}x9 = {num * 9}\n"
-    f"{num}x10 = {num * 10}\n"
+    f"{enfeite}"
+    f"  {num} x  1 = {num * 1}\n"
+    f"  {num} x  2 = {num * 2}\n"
+    f"  {num} x  3 = {num * 3}\n"
+    f"  {num} x  4 = {num * 4}\n"
+    f"  {num} x  5 = {num * 5}\n"
+    f"  {num} x  6 = {num * 6}\n"
+    f"  {num} x  7 = {num * 7}\n"
+    f"  {num} x  8 = {num * 8}\n"
+    f"  {num} x  9 = {num * 9}\n"
+    f"  {num} x 10 = {num * 10}\n"
+    f"{enfeite}"
 )
