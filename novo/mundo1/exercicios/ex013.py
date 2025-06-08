f15c387f18e375b517f941680cc65c4d9d89c877
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-08 11:00:30 -0400

    uptade: money format

--- novo/mundo1/exercicios/ex013.py
+++ novo/mundo1/exercicios/ex013.py
@@ -4,11 +4,11 @@ salário, com aumento de 15%
 """
 
 
-salario = float(input("Qual é o seu salario? "))
+salario = float(input("Qual é o seu salario? R$"))
 
 salario_aumento = salario * 1.15
 
 print(
-    f"Parabéns, seu salario passou de {salario:.2f}"
-    f" para {salario_aumento:.2f}"
+    f"Parabéns, seu salario passou de R${salario:.2f}"
+    f" para R${salario_aumento:.2f}"
 )
