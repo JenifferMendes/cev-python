5b29d5bb2489b5e2e5e3f55725866cfee776f011
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-10 14:49:27 -0400

    fix: position + 1

--- novo/mundo1/exercicios/ex026.py
+++ novo/mundo1/exercicios/ex026.py
@@ -15,6 +15,6 @@ letra_ultimo_a = frase_lower.rfind("a")
 
 print(
     f"A frase: {frase_lower}, contém: {letra_count_a}A, e" 
-    f" o primeiro A aparece na posição: {letra_primeira_a}.\n"
-    f"O último A aparece na posição: {letra_ultimo_a}"
+    f" o primeiro A aparece na posição: {letra_primeira_a + 1}.\n"
+    f"O último A aparece na posição: {letra_ultimo_a + 1}"
 )
