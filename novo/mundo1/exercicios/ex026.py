b6df746e2aa56eca14c71d6b97e858425f388c73
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-10 14:48:33 -0400

    uptade: rfind method

--- novo/mundo1/exercicios/ex026.py
+++ novo/mundo1/exercicios/ex026.py
@@ -10,9 +10,11 @@ frase = input("Digite uma frase: ").strip()
 
 frase_lower = frase.lower()
 letra_count_a = frase_lower.count("a")
-letra_a = frase_lower.find("a")
+letra_primeira_a = frase_lower.find("a")
+letra_ultimo_a = frase_lower.rfind("a")
 
 print(
     f"A frase: {frase_lower}, contém: {letra_count_a}A, e" 
-    f" o primeiro A aparece na posição: {letra_a}."
+    f" o primeiro A aparece na posição: {letra_primeira_a}.\n"
+    f"O último A aparece na posição: {letra_ultimo_a}"
 )
