5c2e95baa7fcb69a2d6ce5390d8af9d14bce8a3c
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-09 23:04:21 -0400

    feat: silva name verify

---
+++ novo/mundo1/exercicios/ex025.py
@@ -0,0 +1,14 @@
+"""
+Crie um programa que leia o nome de uma pessoa e diga se ela tem Silva no nome.
+"""
+
+
+nome = input("Digite seu nome completo: ")
+
+nome_lower = nome.lower()
+silva = "silva" in nome_lower
+
+print(
+    f"Seu nome é: {nome.title()}.\n"
+    f"Seu nome tem Silva? {silva}."
+)
