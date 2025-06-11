43efbf393039a3807d50d8c1cb956f54ff320e27
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-11 16:50:54 -0400

    uptade: time library

--- novo/mundo1/exercicios/ex028.py
+++ novo/mundo1/exercicios/ex028.py
@@ -7,14 +7,21 @@ O programa deverá escrever na tela se o usuário venceu ou perdeu.
 
 
 from random import randint
+from time import sleep
 
 
+print("Vou pensar em um número entre 0 e 5. Tente aivinhar!") 
 numero = int(input("Tente adivinhar o número, digite um número de 0 a 5: "))
+
+print("Loading...")
+
+sleep(2)
 
 numero_pc = randint(0,5)
 
 if numero == numero_pc:
-    print(f"Parabéns o número que você digitou é igual ao do pc: {numero}")
+    print(f"Parabéns, você ganhou: {numero}")
 else:
-    print(f"que pena, o número foi: {numero_pc}")
-print("Adeus")
+    print(
+        f"Ganhei, melhore! O número que pensei foi: {numero_pc} e não {numero}."
+    )
