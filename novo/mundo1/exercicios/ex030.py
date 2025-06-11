5bf6eedb02e3d505f2316966d018ffcd261701a7
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-10 21:41:20 -0400

    feat: even or odd

---
+++ novo/mundo1/exercicios/ex030.py
@@ -0,0 +1,13 @@
+"""
+Crie um programa que leia um número inteiro e mostre na tela se ele é par ou
+impar.
+"""
+
+
+numero = int(input("Digite um número: "))
+
+if numero % 2 == 0:
+    print("O número que você digitou é par")
+else:
+    print("O número que você digitou é impar")
+print("chupa employing")
