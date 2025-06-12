48b540b694c98bb7a9a0ba31628f1a5acf2d3669
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-12 19:48:29 -0400

    feat: check average

---
+++ novo/mundo2/exercicio/ex040.py
@@ -0,0 +1,23 @@
+"""
+Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando
+uma mensagem no final, de acordo com a média atingida:
+- media abaixo de 5.0:
+reprovado
+- media entre 5.0 e 6.9:
+recuperação
+média 7.0 ou superior:
+aprovado.
+"""
+
+
+nota_um = float(input("Digite a nota1: "))
+nota_dois = float(input("Digite a nota2: "))
+
+media = (nota_um + nota_dois) / 2
+
+if media >= 7:
+    print(f"Sua média é: {media}.Parabéns, você foi aprovado.")
+elif media >= 5:
+    print(f"Sua média é: {media} Você ficou de reperação.")
+elif media < 5:
+    print(f"Sua média é: {media}. Você está reprovado.")
