bbe565bc7a547d5a1ae6dbb0068eaf5815d4ba9b
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-12 19:27:29 -0400

    uptade: conversion base working

--- novo/mundo2/exercicio/ex037.py
+++ novo/mundo2/exercicio/ex037.py
@@ -6,18 +6,23 @@ escolher qual será a base de conversão:
 - 2 para octal
 - 3 para hexadecimal
 
-voltar a fazer quando entender as conversoes.(assistir o curso do guanabara.)
 """
 
 numero = int(input("Digite um número: "))
 base = int(input(
     "Qual a base da conversão, digite 1-binário,2-octal,3-hexadecimal" 
-        ))
+))
 
 if base == 1:
-    print("Base de conversão escolhida foi: binário")
+    binario = bin(numero)
+    print(f"Base de conversão escolhida foi: binário, seu número é: {binario}")
 elif base == 2:
-    print("Base de conversão escolhida foi octal")
+    octal = oct(numero)
+    print(f"Base de conversão escolhida foi octal, seu número é: {octal}")
 elif base == 3:
-    print("Base de conversão escolhida foi hexadecimal")
-print("voce digitou os números errados.")
+    hexadecimal = hex(numero)
+    print(
+        f"Base de conversão escolhida foi hex, seu número é: {hexadecimal}"
+    )
+else:
+    print("voce digitou o número errado.")
