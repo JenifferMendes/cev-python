8710734af39fadaa5a180363a9b12b496d917377
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-13 18:01:54 -0400

    fix: elif for else

--- novo/mundo2/exercicio/ex043.py
+++ novo/mundo2/exercicio/ex043.py
@@ -14,15 +14,13 @@ altura = float(input("digite sua altura: "))
 
 imc = peso / (altura ** 2)
 
-if imc < 18.5:
+if imc <= 18.5:
     print(f"Seu imc é: {imc:.0f}. Você está abaixo do peso.")
-elif 18.5 <= imc <= 25:
+elif 18.5 < imc <= 25:
     print(f"Seu imc é: {imc:.0f}. Você está no peso ideal.")
 elif 25 < imc <= 30:
     print(f"Seu imc é: {imc:.0f}. Você esta com sobrepeso.")
 elif 30 < imc <= 40:
     print(f"Seu imc é: {imc:.0f}. Você está com obesidade.")
-elif imc > 40:
-    print(f"Seu imc é: {imc:.0f}. Você está com obesidade mórbida.")
 else:
-    print(f"Digitou algo errado.")
+    print(f"Seu imc é: {imc:.0f}. Você está com obesidade mórbida.")
