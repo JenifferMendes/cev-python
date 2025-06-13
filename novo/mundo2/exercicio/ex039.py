8875386d0533a35b41262b8d9fe607fe726b7539
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-13 19:09:07 -0400

    uptade: gender option

--- novo/mundo2/exercicio/ex039.py
+++ novo/mundo2/exercicio/ex039.py
@@ -4,7 +4,6 @@ com sua idade:
 - se ele ainda vai se alistar ao serviço militar.
 - seé hora de se alistar.
 - se ja passou do tempo do alistamento.
-
 O seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
 """
 
@@ -15,12 +14,30 @@ from datetime import date
 ano = date.today().year
 
 nascimento = int(input("Digite o ano de nascimento: "))
+sexo = int(input( 
+    "Qual é o seu sexo?\n"
+    "[1] Mulher\n" 
+    "[2] Homem\n"
+))
 
-idade = ano - nascimento
-
-if idade == 18:
-    print("Está na hora de servir o exercito.")
-elif idade > 18:
-    print(f"Você tem {idade} anos, já passou {idade - 18} anos.")
-elif idade < 19:
-    print(f"Você ainda não está na idade para servir, falta {18 - idade} anos")
+if sexo == 2:
+    idade = ano - nascimento
+    if idade == 18:
+        print("Está na hora de se alistar.")
+    elif idade > 18:
+        tempo = idade - 18
+        alistar = ano - tempo
+        print(
+            f"Você tem {idade} anos, já passou {tempo} anos. "
+            f"Você deveria se alistar em {alistar}."
+        )
+    else:
+        tempo = 18 - idade
+        alistar = nascimento + 18
+        print(
+            f"Você tem {idade} anos, ainda não está na idade para servir, "
+            f"falta {tempo} anos. "
+            f"Você deverá se alistar em {alistar}."
+        )
+elif sexo == 1:
+    print("Você é mulher e não precisa se alistar.")
