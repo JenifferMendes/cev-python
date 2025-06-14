5b32157f632b116d3cb445c433a91cf373893336
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-14 17:11:14 -0400

    uptade: format

--- novo/mundo2/exercicio/ex046.py
+++ novo/mundo2/exercicio/ex046.py
@@ -6,7 +6,8 @@ fogos de artificio. indo de 10 até 0, com uma pausa de 1 segundo entre eles.
 
 from time import sleep
 
+
 for c in range(10, -1, -1):
     print(c)
     sleep(1)
-print("Feliz ano novo!")
+print("KABOOM 🎆")
