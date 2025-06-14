9a908ce7a7fb76aa055ae109914446741a5fd234
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-14 11:24:33 -0400

    feat: show odd from 1 to 50.

---
+++ novo/mundo2/exercicio/ex047.py
@@ -0,0 +1,9 @@
+"""
+Crie um progama que mostre na tela todos os números pares
+que estão no intervalo entre 1 e 50.
+"""
+
+
+for c in range(0, 50, 2):
+    print(c)
+print("todos os pares entre 1 e 50.")
