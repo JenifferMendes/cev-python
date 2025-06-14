a6aa0825b84bf1bc95de3c58ea8c63453b4a97dd
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-14 17:24:19 -0400

    uptade: format

--- novo/mundo2/exercicio/ex048.py
+++ novo/mundo2/exercicio/ex048.py
@@ -5,9 +5,10 @@ impares que são multiplos de tres e que se encontram no intervalo de 1 até 500
 
 
 soma = 0
-
+numeros = 0
 for c in range(1, 500, 2):
     if c % 3 == 0:
         soma = soma + c
-        print(c)
-print(f"Todos os números multiplos de 1 a 500. A soma {soma}.")
+        numeros = numeros + 1
+        #print(c, numeros)
+print(f"Todos os {numeros} números multiplos de 1 a 500 tem a soma de: {soma}.")
