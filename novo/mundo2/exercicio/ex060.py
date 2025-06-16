37619b35f9b895ac94ce578ba17faeb087f11729
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-16 18:38:21 -0400

    uptade: factorial verify using while and for

--- novo/mundo2/exercicio/ex060.py
+++ novo/mundo2/exercicio/ex060.py
@@ -3,3 +3,18 @@ faça um programa que leia um número qualquer e mostre o seu fatorial.
 5! = 5x4x3x2x1 = 120
 tente fazer com for e while
 """
+
+
+numero = numero_for = int(input("Digite um número: "))
+
+fatorial = fatorial_for= 1
+print(f"O fatorial de {numero} ", end="")
+while numero != 1:
+    fatorial *= numero
+    numero -= 1
+print(f"é igual a {fatorial} usando o while.")
+
+for c in range(1, numero_for + 1):
+    fatorial_for *= c
+    #print(c,fatorial_for, numero_for)
+print(f"Com o for, o fatorial de {numero_for} é {fatorial_for}.")
