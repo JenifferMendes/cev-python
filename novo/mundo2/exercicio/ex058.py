7f809e7172359968fd8bc24e305a7e249d23af77
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-15 21:30:23 -0400

    feat: subject

---
+++ novo/mundo2/exercicio/ex058.py
@@ -0,0 +1,5 @@
+"""
+melhore o jogo desafio 028, onde o computador vai pensar em um numero entre 0 e
+10. so que agora o jogador vai tentar adivinhar ate acertar, mostrando
+no final quantos palpites foram necessarios para vencer.
+"""
