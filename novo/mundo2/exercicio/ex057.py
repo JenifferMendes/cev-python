9e9d6d5a677bfd84d58ffeb9878646debfe8a481
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-16 16:20:41 -0400

    uptade: gender verify

--- novo/mundo2/exercicio/ex057.py
+++ novo/mundo2/exercicio/ex057.py
@@ -1,4 +1,15 @@
 """
 Faça um programa que leia o sexo de uma pessoa, mas só aceite valores M ou F.
 caso esteja errado, peça a digitação novamente até ter um valor correto.
+parei aqui
 """
+
+
+resposta = "H M"
+while "M" in resposta or "H" in resposta:
+    resposta = input(
+        "Digite uma das opçoes abaixo:\n"
+        "[M] para Mulher\n"
+        "[H] para Homem \n"
+    ).upper()
+print("Fim")
