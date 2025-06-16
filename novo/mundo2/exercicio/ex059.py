efb047711b43f34fb38e6ed0672f3a45e50aac06
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-15 21:31:39 -0400

    feat: subject

---
+++ novo/mundo2/exercicio/ex059.py
@@ -0,0 +1,10 @@
+"""
+Crie um programa que leia dois valores e mostre um menu na tela
+[1]somar
+[2]multiplicar
+[3]maior
+[4]novos números
+[5]sair do programa
+
+seu programa devera realizar a operaçao solicitada em caada caso
+"""
