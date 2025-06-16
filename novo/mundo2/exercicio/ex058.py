eab7ec34198bf9c8e164248072cacd94c54367d9
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-16 16:34:21 -0400

    uptade: think a number game

--- novo/mundo2/exercicio/ex058.py
+++ novo/mundo2/exercicio/ex058.py
@@ -3,3 +3,25 @@ melhore o jogo desafio 028, onde o computador vai pensar em um numero entre 0 e
 10. so que agora o jogador vai tentar adivinhar ate acertar, mostrando
 no final quantos palpites foram necessarios para vencer.
 """
+
+
+from random import randint
+
+
+print("Bem vindo ao jogo tente adivinhar um número.")
+print("Tente adivinhar o número que estou pensando! um número entre 0 a 10.")
+
+numero_pc = randint(0,10)
+numero_jogador = 0
+palpites = 0
+while numero_pc != numero_jogador:
+    numero_jogador = int(input("De um palpite:"))
+    if numero_jogador > 10:
+        print("Você digitou um número errado. Tente novamente")
+    if numero_jogador <= 10:
+        if numero_jogador!= numero_pc:
+            print("tente novamente!")
+            palpites += 1
+            print(palpites, numero_pc)
+print(f"Você escolheu {numero_jogador} e o Computador {numero_pc}.")
+print(f"Você acertou!! O número total de palpites foi de {palpites} vezes.")
