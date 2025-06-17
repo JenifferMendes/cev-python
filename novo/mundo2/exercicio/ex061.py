2f3c3e990a30b65b0a9d4c7bc898c73d05fecc8a
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-17 15:04:06 -0400

    uptade: ap verify with while

--- novo/mundo2/exercicio/ex061.py
+++ novo/mundo2/exercicio/ex061.py
@@ -1,6 +1,20 @@
 """
 refaça o desafio 51, lendo o primeiro termo e a razão de uma pa.
 mostrando os 10 primeiros termos da progressao usando a estrutura while.
-
-
 """
+
+
+primeiro_termo = int(input("Digite o primeiro termo da sua PA: "))
+razao = int(input("Digite a razao da sua PA: "))
+
+print(
+    f"O primeiro da sua PA é {primeiro_termo}.\n"
+    f"A razão da sua PA é {razao}."
+)
+
+opcao = 1
+pa = [primeiro_termo]
+while opcao != 10:
+    pa.append(primeiro_termo + (opcao * razao))
+    opcao += 1
+print(f"Sua PA é {pa}")
