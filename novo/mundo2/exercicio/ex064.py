1506ebac8104477d393d9ec9cd0cb5aa8753f3be
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-17 15:02:54 -0400

    uptade: sum number verify

--- novo/mundo2/exercicio/ex064.py
+++ novo/mundo2/exercicio/ex064.py
@@ -4,3 +4,16 @@ o programa so vai aparar quando o usuario digitar o valor 999, que é a condiç�
 de parada. no final, mostre quantos numeros foram digitados e qual foi a soma
 entre eles, desconsiderando o flag.(o 9990)
 """
+
+numero = 0
+soma = 0
+quantidade = 0
+while numero != 999:
+    numero = int(input("digite um número: "))
+    if numero != 999:
+        soma += numero
+        quantidade += 1
+        print(soma, quantidade)
+print(
+    f"Você digitou {quantidade} números e a soma de todos eles é: {soma}."
+)
