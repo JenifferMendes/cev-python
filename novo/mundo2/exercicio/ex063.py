4c9692ce36b7f6bc2ff5d9768677a8737064ed67
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-17 15:03:18 -0400

    uptade: fibonacci verify

--- novo/mundo2/exercicio/ex063.py
+++ novo/mundo2/exercicio/ex063.py
@@ -4,3 +4,18 @@ n primeiros elementos de uma sequencia de fibonacci
 ex
 0 -> 1 -> 1 -> 2 -> 3 -> 5 -> 8
 """
+
+
+numero = int(input(
+    "Digite a quantidade de números da sequência de Fibonacci: ")
+)
+fibonacci = [0, 1]
+
+while len(fibonacci) < numero:
+    proximo = fibonacci[-1] + fibonacci[-2]
+    fibonacci.append(proximo)
+
+if numero == 1:
+    print("Sua sequência de Fibonacci [0].")
+else:
+    print(f"Sua sequência de fibonacci é {fibonacci}.")
