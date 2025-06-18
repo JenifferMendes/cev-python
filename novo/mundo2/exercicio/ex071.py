eb8d27b54e1f4c7de1b856fb3037b401d33440d2
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-18 13:01:16 -0400

    feat: statement

---
+++ novo/mundo2/exercicio/ex071.py
@@ -0,0 +1,7 @@
+"""
+Crie um programa que simule o funcionamento de um caixa eletrônico. No inicio,
+pergunta se o usuario qual será o valor a ser sacado (numero inteiro) e o
+programa vai informar quantas cedulas de cada valor serao entregues.
+considerando que o caixa possui cedulas de 50, 20, 10 e 1.
+
+"""
