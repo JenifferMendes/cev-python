d97943b06988f18067d51ce533b1fa388728d6e8
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-18 09:27:40 -0400

    uptade: format

--- novo/mundo2/exercicio/ex064.py
+++ novo/mundo2/exercicio/ex064.py
@@ -5,15 +5,12 @@ de parada. no final, mostre quantos numeros foram digitados e qual foi a soma
 entre eles, desconsiderando o flag.(o 9990)
 """
 
-numero = 0
-soma = 0
-quantidade = 0
+numero = soma = quantidade = 0
 while numero != 999:
-    numero = int(input("digite um número: "))
+    numero = int(input("digite um número:[999 para parar] "))
     if numero != 999:
         soma += numero
         quantidade += 1
-        print(soma, quantidade)
 print(
     f"Você digitou {quantidade} números e a soma de todos eles é: {soma}."
 )
