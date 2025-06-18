60c76f929c27d0fdc7ce1fbfa0bc2ab2e657d930
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-18 13:00:51 -0400

    feat: statement

---
+++ novo/mundo2/exercicio/ex069.py
@@ -0,0 +1,8 @@
+"""
+Crie um programa que leia a idade e o sexo de várias pessoas. A cada pessoa
+cadastrada, o programa pergunta se o usuário quer ou não continuar.
+No final, mostre:
+a) quantas pessoas tem mais de 18 anos.
+b) quantos homens foram cadastrados.
+c) quantas mulheres tem menos de 20 anos.
+"""
