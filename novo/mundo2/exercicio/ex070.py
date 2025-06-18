89e6560325b277008ec08e27d3bce68b8cd970e6
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-18 13:01:07 -0400

    feat: statement

---
+++ novo/mundo2/exercicio/ex070.py
@@ -0,0 +1,8 @@
+"""
+Crie um programa que leia o nome e o preço de vários produtos. o programa deverá
+perguntar se o usuário vai continuar. No final, mostre:
+a) qual é o total gasto na compra.
+b)quantos produtos custam mais de 1000.
+c) qual é o nome do produto mais barato.
+"""
+
