3b3cfca0f1d931fcadf910b4253114a511627041
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-18 11:23:02 -0400

    feat: sum verify

---
+++ novo/mundo2/exercicio/ex066.py
@@ -0,0 +1,17 @@
+"""
+Crie um programa que leia vários números inteiros pelo teclado.
+O programa só vai parar quando o usuario digitar o valor 999, que é a condição
+de parada. no final, mostre quantos números foram digitados e qual foi a soma
+entre eles(desconsiderando a flag).
+"""
+
+
+soma = quantidade = 0
+
+while True:
+    numero = int(input("Digite um número: "))
+    if numero == 999:
+        break
+    soma += numero
+    quantidade += 1
+print(f"Você digitiou {quantidade} e a soma entre eles {soma}.")
