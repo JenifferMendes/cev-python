28da8edb4042c2251ce24448a9054f7ec1f1f128
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-19 13:16:29 -0400

    refact: removing unnecessary condition

--- novo/mundo2/exercicio/ex067.py
+++ novo/mundo2/exercicio/ex067.py
@@ -4,26 +4,24 @@ para cada valor digitado pelo usuário. O programa será interrompido quando o
 número solicitado for negativo.
 """
 
-dash = "\n" + "="* 25 + "\n"
 
-print(dash)
+dash = "\n" + "="* 25 + "\n"
+count = 1
 
 numero = int(input("Digite um número: "))
 
-count = 1
-
+print(dash)
 
 while True:
+    if numero < 0:
+        break
     print(dash)
     print(f"TABUADA DO {numero}\n")
     while count <= 10:
         tabuada = numero * count 
         print(f" {numero} x {count} = {tabuada}")
         count += 1
-    if count > 10:
-        print(dash)
-        numero = int(input("Digite um novo número: "))
-        count = 1
-    if numero < 0:
-        break
+    print(dash)
+    numero = int(input("Digite um novo número: "))
+    count = 1
 print(f"Fim")
