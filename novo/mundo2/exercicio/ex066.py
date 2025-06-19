fb54c3be294c75e3006babb698f8ffa43c47ccd4
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-19 13:06:50 -0400

    uptade: format

--- novo/mundo2/exercicio/ex066.py
+++ novo/mundo2/exercicio/ex066.py
@@ -9,9 +9,9 @@ entre eles(desconsiderando a flag).
 soma = quantidade = 0
 
 while True:
-    numero = int(input("Digite um número: "))
+    numero = int(input("Digite um número: [999 para parar] "))
     if numero == 999:
         break
     soma += numero
     quantidade += 1
-print(f"Você digitiou {quantidade} e a soma entre eles {soma}.")
+print(f"Você digitou {quantidade} números e a soma entre eles {soma}.")
