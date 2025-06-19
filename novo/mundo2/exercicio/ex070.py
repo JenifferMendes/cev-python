16668ae7396f7c9de39f6152e2ba7657ec5c8010
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-19 18:41:11 -0400

    uptade: format

--- novo/mundo2/exercicio/ex070.py
+++ novo/mundo2/exercicio/ex070.py
@@ -8,18 +8,16 @@ c) qual é o nome do produto mais barato.
 
 
 n = 49
-dash = "~" * n
+dash = "\n" + "~" * n
 dash_espaco = "\n" + "=" * n + "\n"
 
-print(dash_espaco)
-print(f"{'ATACADÃO K-JOTUME':^{n}}")
-print(dash_espaco)
+print(dash_espaco, f"{'ATACADÃO K-JOTUME':^{n}}", dash_espaco)
 
-nome_barato = quantidade = soma = maior = barato = 0
+nome_barato = " "
+quantidade = soma = maior = barato = 0
 
 while True:
-    print(f"{'PASSANDO PRODUTO':^{n}}")
-    print(dash)
+    print(f"{'PASSANDO PRODUTO':^{n}}", dash)
 
     nome_produto = str(input("Digite o nome do produto: "))
     preco_produto = float(input("Digite o preço do produto: R$"))
@@ -27,31 +25,32 @@ while True:
     soma += preco_produto
     quantidade += 1
 
-    if quantidade == 1:
-        barato = preco_produto
-
-    if preco_produto < barato:
+    if quantidade == 1 or preco_produto < barato:
         barato = preco_produto
         nome_barato = nome_produto
 
     if preco_produto > 1000:
         maior += 1
 
-    opcao = input(
-        "Deseja adicionar mais alguma coisa? [S/N]"
-    ).strip().upper()[0]
-    
+    continuar = ["S", "N"]
+    opcao = " "
+    while opcao not in continuar:
+        opcao= input(
+            "Deseja adicionar mais alguma coisa? [S/N]"
+        ).strip().upper()
     if opcao == "N":
         break
 
     print(dash)
 
-print(dash_espaco)
 print(
-    f"O total da compra foi de: {soma}.\n"
-    f"O produto mais barato foi: {nome_barato}.\n"
-    f"Foram {maior} produtos com mais de R$1000.\n"
-    "Obrigada por comprar com Atacadão K-JOTUME.\n"
-    "Volte sempre!"
+    dash_espaco,
+    (
+        f"\bO total da compra foi de: {soma}.\n"
+        f"O produto mais barato foi: {nome_barato}.\n"
+        f"Foram {maior} produtos com mais de R$1000.\n"
+        "Obrigada por comprar com Atacadão K-JOTUME.\n"
+        "Volte sempre!"
+    ),
+    dash_espaco,
 )
-print(dash_espaco)
