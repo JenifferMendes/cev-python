00abc62428b73db78b2fe80ae6f516eaf39614f7
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-19 12:26:16 -0400

    uptade: banco jotume

--- novo/mundo2/exercicio/ex071.py
+++ novo/mundo2/exercicio/ex071.py
@@ -3,5 +3,29 @@ Crie um programa que simule o funcionamento de um caixa eletrônico. No inicio,
 pergunta se o usuario qual será o valor a ser sacado (numero inteiro) e o
 programa vai informar quantas cedulas de cada valor serao entregues.
 considerando que o caixa possui cedulas de 50, 20, 10 e 1.
-
 """
+
+
+n = 49
+dash = "\n" + "=" * n + "\n"
+
+print(dash)
+print(f"{'Banco Jotume':^{n}}")
+print(dash)
+
+sacar = int(input("Qual valor você deseja sacar? R$"))
+
+notas = [50, 20, 10, 5, 1]
+
+for nota in notas:
+    quantidade = sacar // nota
+    resto = sacar % nota
+    sacar = resto
+
+    if quantidade > 0:
+        print(f"A quantidade de notas de {nota} é {quantidade}.")
+
+print(dash)
+print(f"{'Obrigado por utilizar nosso banco!':^{n}}")
+print(f"{'Banco Jotume':^{n}}")
+print(dash)
