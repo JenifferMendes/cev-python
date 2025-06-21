0a1bfb5795d1acd27da1d3814139254887be0261
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-20 21:58:57 -0400

    feat: statement

---
+++ novo/mundo3/exercicios/ex076.py
@@ -0,0 +1,5 @@
+"""
+Crie um programa que tenha uma tupla única com nomes de produtos e seus
+respectivos preços, na sequencia.
+No final, mostre uma listagem de preços. Organizando os dados em forma tabular.
+"""
