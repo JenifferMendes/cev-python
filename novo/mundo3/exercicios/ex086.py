f05f85a62525cb57ca825eeffe2fa23c1a18e43d
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-23 18:26:56 -0400

    uptade: fix position number

--- novo/mundo3/exercicios/ex086.py
+++ novo/mundo3/exercicios/ex086.py
@@ -9,20 +9,28 @@ matrix = []
 linha_um = []
 linha_dois = []
 linha_tres = []
+posicao =  0
+dash = "\n"+ "-" * 50 + "\n"
 
 for num in range(0, 9):
+    numero = int(input(f"Digite um número: [0, {posicao}]: "))
+    posicao += 1
+    if posicao == 3:
+        posicao = 0
+    if numero % 2 == 0:
+        pares_soma += numero
     if num < 3:
-        linha_um.append(int(input(f"Digite um número: [0, {num}]: ")))
+        linha_um.append(numero)
     elif num < 6:
-        linha_dois.append(int(input(f"Digite um número: [1, {num}]: ")))
+        linha_dois.append(numero)
     else:
-        linha_tres.append(int(input(f"Digite um número: [2, {num}]: ")))
+        linha_tres.append(numero)
 
 matrix.append(linha_um)
 matrix.append(linha_dois)
 matrix.append(linha_tres)
 
-dash = "\n"+ "-" * 50 + "\n"
+print(dash)
 
 for posicao, numero in enumerate(matrix):
     print(f"[ {numero[0]} ] [ {numero[1]} ] [ {numero[2]} ]")
