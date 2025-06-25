c16858c53f120734f63626b0dcd88a84e04bd0ef
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-25 17:30:09 -0400

    feat: statement

---
+++ novo/mundo3/exercicios/ex094.py
@@ -0,0 +1,9 @@
+"""
+Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os
+dados de cada pessoa em um dicionario e todos os dicionarios em uma lista.
+no final, mostre:
+a) quantas pessoas foram cadastradas.
+b) a media de idade do grupo.
+c) uma lista com todas as mulheres.
+d) uma lista com todas as pessoas com idade acima da media.
+"""
