37f06cb3b8c12cb314123e89e015dc9a58facce8
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-25 17:30:03 -0400

    feat: statement

---
+++ novo/mundo3/exercicios/ex092.py
@@ -0,0 +1,7 @@
+"""
+Crie um programa que leia nome, ano de nascimento e carteira de trabalho e
+cadastre-os (com idade)em um dicionário se por acaso a ctps for diferente de
+zero, o dicionário receberá também o ano de contratação e o salario.
+calcule a acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
+35 anos de contribuição para aposentar.
+"""
