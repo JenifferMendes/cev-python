b20f1d47424607dc4c01f37d6cc2f55637c808f4
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-25 17:30:01 -0400

    feat: statement

---
+++ novo/mundo3/exercicios/ex091.py
@@ -0,0 +1,5 @@
+"""
+Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios.
+guarde esses resultados em um dicionario. no final, coloque esse dicionario
+em ordem, sabendo que o vencedor tirou o maior numero no dado.
+"""
