cc09184eb1dba0cb0762db70b0482a89996f68e7
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-26 19:38:33 -0400

    feat: implement student grading program with dictionary storage

--- novo/mundo3/exercicios/ex090.py
+++ novo/mundo3/exercicios/ex090.py
@@ -2,3 +2,21 @@
 Faça um programa que leia nome e média de um aluno, guardando tambéma. situação
 em um dicionário. no final mostre o conteudo da estrutura na tela.
 """
+
+
+aluno = {}
+
+nome = input("Nome: ")
+aluno["Aluno"] = nome
+media = float(input(f"Média de {nome}: "))
+aluno["Média"] = media
+
+if media > 70:
+    aluno["Situação"] = "Aprovado"
+elif 49 < media < 70:
+    aluno["Situação"] = "Recuperação"
+else:
+    aluno["Situação"] = "Reprovado"
+
+for k, v in aluno.items():
+        print(f"{k} é igual a {v}")
