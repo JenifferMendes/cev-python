551ca000bea7d3a6eff652c3245b8debc6ef469c
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-06-29 20:02:56 -0400

    uptade: format

--- novo/mundo3/exercicios/ex092.py
+++ novo/mundo3/exercicios/ex092.py
@@ -9,4 +9,5 @@ calcule a acrescente, além da idade, com quantos anos a pessoa vai se aposentar
 
 nome = input("Digite o nome: ")
 nascimento = int(input("Digite o ano nascimento: "))
-
+contratacao = int(input("Digite ano da contrataçao: "))
+salario = float(input("Digite seu salario: "))
