3873bc9e590ee5888c8c51b05e945c7c3dd6f55a
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-07-26 15:59:32 -0400

    feat: done

---
+++ novo/mundo3/aulas/aula20.py
@@ -0,0 +1,19 @@
+""""
+Funções - parte 01
+"""
+""" n = 40
+
+def titulo(msg):
+    print('-' * n)
+    print(f"{msg:^{n}}")
+    print('-' * n)
+
+
+titulo("BTS")
+titulo("Stray Kids")
+titulo("Ateez")
+ """
+
+
+for c in range(10, -1, -1):
+    print(c)
