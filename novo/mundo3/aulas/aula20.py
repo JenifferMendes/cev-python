eb06e46943ff0618391f4916a1565f8b0dd59b64
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-09-22 18:01:18 -0400

    uptade: reassistindo aula 20 do guanabara até 20min

--- novo/mundo3/aulas/aula20.py
+++ novo/mundo3/aulas/aula20.py
@@ -12,6 +12,38 @@ def titulo(msg):
 titulo("BTS")
 titulo("Stray Kids")
 titulo("Ateez")
+
+22/09/2025
+
+funcoes - parte 01
+
+rotina.
+o python tem varias funcoes que vem com ele como :print(), len(), input(), 
+int(), float().... são funções
+
+def - define a função
+cria a função
+pule duas linhas abaixo 
+voce pode usar parametros na função para ficar ainda mais personalizado sua função.
+
+def mostraLinha():
+    print("-------------------")
+
+
+mostraLinha()
+print("sistema de alunos")
+mostraLinha()
+
+
+def mensagem(msg):
+    print("----------------")
+    print(msg)
+    print("----------------")
+
+mensagem("SISTEMA DE ALUNOS")
+
+
+até 20:13
  """
 
 
