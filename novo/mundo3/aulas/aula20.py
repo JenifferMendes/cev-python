27916440d6f6139b3dff7535b2b0ffdb29a58548
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-09-23 16:50:31 -0400

    uptade: finalizando a aula 20 do mundo 03 + exercicios(reassisti a solução)

--- novo/mundo3/aulas/aula20.py
+++ novo/mundo3/aulas/aula20.py
@@ -44,6 +44,57 @@ mensagem("SISTEMA DE ALUNOS")
 
 
 até 20:13
+
+23/09/2025
+
+funções são rotinas
+
+a = 4
+b = 5
+s = a + b
+print(s)
+
+a = 8
+b = 9
+s = a + b
+print(s)
+
+a = 2
+b = 1
+s = a + b
+print(s)
+
+def soma(a,b):
+    s = a + b
+    print(s)
+
+
+# programa principal
+soma(4,5)
+soma(8,9)
+soma(2,1)
+
+def contador(* num):
+    tam = len(num)
+    print(f"Recebe os valores {num} e são ao todo {tam} números ")
+
+
+contador(2,1,7)
+contador(8,0)
+contador(4,4,7,6,2)
+
+def dobra(list):
+    pos = 0
+    while pos < len(list):
+        list[pos] *= 2
+        pos +=1
+
+valores = [1,2,3,4,5]
+dobra(valores)
+print(valores)
+
+-> fiz os exercicios já.
+revi as resolução do 97 até 100
  """
 
 
