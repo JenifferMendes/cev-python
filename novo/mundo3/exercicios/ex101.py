ed162a26c92ac8f072bd950d3982ad41c87f0b13
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-09-26 13:49:26 -0400

    uptade: arrumei a função e as variaveis.

--- novo/mundo3/exercicios/ex101.py
+++ novo/mundo3/exercicios/ex101.py
@@ -8,17 +8,19 @@ indicando se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições
 from datetime import date
 
 
+
+ano = int(input("Em que ano você nasceu? "))
+idade = date.today().year - ano
+
 def voto(ano):
-    idade = date.today().year - ano
     if idade < 16:
         valor = 'NEGADO'
     elif 18 > idade >= 16 or idade > 70:
         valor = 'VOTO OPCIONAL'
     else:
         valor = 'VOTO OBRIGATÓRIO'
-    print(f"Com {idade} anos: ", end="")
     return valor
 
 
-ano = int(input("Em que ano você nasceu? "))
-print(f"{voto(ano)}.")
+resultado = voto(ano)
+print(f"Com {idade} anos: {resultado}")
