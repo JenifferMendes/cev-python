ddd1632a8af03701f843cfe99540722963b660ab
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-10-02 22:08:41 -0400

    feat: arquivo do programa principal exercicio 111

---
+++ novo/mundo3/exercicios/ex111/ex111.py
@@ -0,0 +1,14 @@
+""""
+Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(),
+diminuir(), dobro(), e metade().
+
+Faça também um programa que importe esse módulo e use algumas dessas funções.
+"""
+
+
+from utilidadesCev import dado, moeda
+
+
+b = float(input("Digite o preço: R$"))
+
+dado.resumo(b)
