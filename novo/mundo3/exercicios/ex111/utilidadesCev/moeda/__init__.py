d2fc955fd7676a0eccf6017177d7decf7a56bd4d
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-10-06 17:47:39 -0400

    uptade: transferencia de função resumo dados para moeda

--- novo/mundo3/exercicios/ex111/utilidadesCev/moeda/__init__.py
+++ novo/mundo3/exercicios/ex111/utilidadesCev/moeda/__init__.py
@@ -20,3 +20,17 @@ def metade(valor=0, format=False):
 
 def moeda(valor = 0, moeda = "R$"):
     return f"{moeda}{valor:>.2f}".replace(".",",")
+
+
+def resumo(valor=0, taxaa=10, taxab=5):
+    n = 35
+    dash = "-" * n
+    print(dash)
+    print("RESUMO DO VALOR".center(30))
+    print(dash)
+    print(f"Preço analisado: \t{moeda(valor)}")
+    print(f"Dobro do preço: \t{dobrar(valor, True)}")
+    print(f"Metade do preço: \t{metade(valor, True)}")
+    print(f"{taxaa}% de aumento: \t{aumentar(valor, taxaa, True)}")
+    print(f"{taxab}% de redução: \t\t{diminuir(valor, taxab, True)}")
+    print(dash)
