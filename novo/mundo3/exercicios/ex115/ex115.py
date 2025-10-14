c72c68b4179ed6240e764d4ab4d1218158134a31
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-10-14 04:52:32 -0400

    feat: comecei a montar o sistema do exercicio 115

---
+++ novo/mundo3/exercicios/ex115/ex115.py
@@ -0,0 +1,15 @@
+"""
+Crie um pequeno sistema modularizado que permite cadastrar pessoas pelo seu
+nome e idade em um arquivo de texto simples.
+
+O sistema só vai ter 2 opções: cadastrar uma nova pessoa e listar todas as 
+pessoas cadastradas.
+"""
+
+
+import lib.interface
+
+
+
+lib.interface.titulo("SISTEMA ARQUIVO V1.0")
+lib.interface.menu(["opcao ", "opcao" , "opcao"])
